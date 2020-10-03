37fab1c3d6e3c7c61be1cedecb9b5f93c46afb3c
M.Zaki Al Akkari <56824972+MrZakiakkari@users.noreply.github.com>
2020-10-04 00:35:24 +0100

    refactoring

--- src/main/java/github/mrzakiakkari/LondonTubeStatistics.java
+++ src/main/java/github/mrzakiakkari/LondonTubeStatistics.java
@@ -32,6 +32,7 @@ public class LondonTubeStatistics
 	{
 
 		fillData(data);
+		fillDataArrayWithGeneratedStatistics(data);
 
 		printData(data);
 
@@ -44,26 +45,29 @@ public class LondonTubeStatistics
 	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	private void fillData(int[][] data)
 	{
-		for (int row = 0; row < data.length; row++)
+		for (int[] data1 : data)
 		{
 			for (int col = 0; col < headers.length; col++)
 			{
 				switch (col)
 				{
 					case 0:
-					case 3://weekday
-						data[row][col] = ThreadLocalRandom.current().nextInt(50000, 140000 + 1);
+					case 3:
+						//weekday
+						data1[col] = ThreadLocalRandom.current().nextInt(50000, 140000 + 1);
 						break;
 					case 1:
-					case 4: //Saturday
-						data[row][col] = ThreadLocalRandom.current().nextInt(30000, 80000 + 1);
+					case 4:
+						//Saturday
+						data1[col] = ThreadLocalRandom.current().nextInt(30000, 80000 + 1);
 						break;
-					default: //Sunday
-						data[row][col] = ThreadLocalRandom.current().nextInt(5000, 15000 + 1);
+					default:
+						//Sunday
+						data1[col] = ThreadLocalRandom.current().nextInt(5000, 15000 + 1);
 						break;
-				}//end switch
-			}//end for
-		}//end for
+				} //end switch
+			} //end for
+		} //end for
 
 	}//end fillData()
 
@@ -180,4 +184,107 @@ public class LondonTubeStatistics
 
 	}//end method findPercChange()
 
-}//end class
+	public String printOutput(int[][] stationsDataArray)
+	{
+		String text = getHeader1()
+				+ getHeader2()
+				+ getStations(stationsDataArray)
+				+ getBusiestStations(stationsDataArray)
+				+ getHeader3()
+				+ getPercentageDifferences(stationsDataArray);
+
+		return text;
+	}
+
+	private void fillDataArrayWithGeneratedStatistics(int[][] stationsDataArray)
+	{
+		for (int[] stationData : stationsDataArray)
+		{
+			for (int dayIndex = 0; dayIndex < stationData.length; dayIndex++)
+			{
+				stationData[dayIndex] = generateStatistic(dayIndex);
+			}
+		}
+	}
+
+	private int generateStatistic(int dayIndex)
+	{
+		switch (dayIndex)
+		{
+			case yearFirstHalfWeekday:
+			case yearSecondHalfWeekday:
+				return ThreadLocalRandom.current().nextInt(weekdayLowerBound, weekdayUpperBound + 1);
+			case 1:
+			case 4:
+				//Saturday
+				return ThreadLocalRandom.current().nextInt(30000, 80000 + 1);
+			default:
+				//Sunday
+				return ThreadLocalRandom.current().nextInt(5000, 15000 + 1);
+		}
+	}
+	private final int yearFirstHalfWeekday = 0;
+	private final int yearSecondHalfWeekday = 3;
+	private final int weekdayLowerBound = 5000;
+	private final int weekdayUpperBound = 140000;
+
+	private String getHeader1()
+	{
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	private String getHeader2()
+	{
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	private String getStations(int[][] stationsDataArray)
+	{
+		String text = "";
+		for (int stationIndex = 0; stationIndex < stationsDataArray.length; stationIndex++)
+		{
+			String name = stations[stationIndex];
+			int[] stationData = stationsDataArray[stationIndex];
+
+			text += getStationText(name, stationData);
+		}
+		return text;
+	}
+
+	private String getBusiestStations(int[][] stationsDataArray)
+	{
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	private String getHeader3()
+	{
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	private String getPercentageDifferences(int[][] stationsDataArray)
+	{
+		double[] percentages = calculatePercentageDifferences(stationsDataArray);
+		String text = "";
+		for (int index = 0; index < percentages.length; index++)
+		{
+			text += stations[index] + ":" + percentages[index] + System.lineSeparator();
+		}
+		return text;
+
+	}
+
+	private String getStationText(String name, int[] stationData)
+	{
+		String text = name + "\t";
+		for (int index = 0; index < stationData.length; index++)
+		{
+			text = text + stationData[index] + "\t" + System.lineSeparator();;
+		
+		return text;
+	}
+
+	private double[] calculatePercentageDifferences(int[][] stationsDataArray)
+	{
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+}
